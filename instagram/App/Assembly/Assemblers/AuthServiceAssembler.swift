db97243fb0d4964d929c776844333e632d3e3276
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-08-20 11:43:54 +0700

    Настроены зависимости для экрана авторизации

--- instagram/App/Assembly/Assemblers/AuthServiceAssembler.swift
+++ instagram/App/Assembly/Assemblers/AuthServiceAssembler.swift
@@ -7,5 +7,14 @@ import Swinject
 
 /// Сборщик сервиса аутентификации
 class AuthServiceAssembler: Assembly {
-    func assemble(container: Container) { }
+    func assemble(container: Container) {
+        
+        container.register(AbstractAuthViewModel.self) { _ in
+            return AuthViewModel()
+        }
+        
+        container.register(AbstractAuthViewRouter.self) { _ in
+            return AuthViewRouter()
+        }
+    }
 }
