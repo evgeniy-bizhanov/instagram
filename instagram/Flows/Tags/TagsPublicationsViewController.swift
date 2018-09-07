2030315a503c58934222a47d6f9754aa879690d4
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-09-07 10:18:35 +0700

    Добавлен контроллер публикаций по тегу

---
+++ instagram/Flows/Tags/TagsPublicationsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Показывает публикации по тегу
+//
+
+import UIKit
+
+class TagsPublicationsViewController: UITableViewController {
+    
+    // MARK: - IBOutlets
+    // MARK: - Models
+    // MARK: - Properties
+    // MARK: - Fields
+    // MARK: - IBActions
+    // MARK: - Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: - Services
+    // MARK: - Initializers
+}
